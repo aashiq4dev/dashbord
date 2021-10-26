f90fe4b396164e40550f303b905d53d18c779b4c
AASHIQ <85242003+aashiq4dev@users.noreply.github.com>
2021-10-26 01:25:47 -0700

    routes setup as list

---
+++ lib/routing/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const OverViewPageRoutes = 'OverView';
+const DriversPageRoutes = 'Drivers';
+const ClientsPAgeRoutes = 'Clients';
+const AuthenticationPAgeRoutes = 'Authentication';
+
+List siteMenuItems = [
+  OverViewPageRoutes,
+  DriversPageRoutes,
+  ClientsPAgeRoutes,
+  AuthenticationPAgeRoutes,
+];
